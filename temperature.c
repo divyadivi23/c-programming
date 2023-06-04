0d9bd347d7f63c59f203d01c1d29e49ef5c283d6
divyadivi23 <132284300+divyadivi23@users.noreply.github.com>
2023-06-04 18:55:46 +0530

    Add files via upload

---
+++ temperature.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    float temperature;
+
+    printf("Enter the current temperature in degrees Celsius: ");
+    scanf("%f", &temperature);
+
+    if (temperature > 30.0) {
+        printf("Temperature exceeds safe limits.\n");
+    }
+    else if(temperature <30.0) {
+    	printf("Temperature does not exceeds safe limits.\n");
+    }
+    	
+	
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	printf("a is %d,b is %d,c is %d which are 2 digit",a,b,c);
+	
+	return 0;
+	
+}
